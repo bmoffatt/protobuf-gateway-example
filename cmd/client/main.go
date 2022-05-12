b3f200ad2f0907275bbeebd05d1f8bbe8297776a
Bryan Moffatt <bmoff2292@gmail.com>
2022-05-11 21:25:34 -0700

    show error details too

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/bmoffatt/protobuf-gateway-example/pb/v2"
 	"github.com/google/uuid"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
 func init() {
@@ -18,12 +19,20 @@ func main() {
 	log.Print("hello world!")
 	conn := must.Return(grpc.Dial("localhost:9000", grpc.WithInsecure()))
 	client := pb.NewGatewayClient(conn)
-	log.Print(must.Return(client.DoTheThing(context.TODO(), &pb.GatewayRequest{
+	response, err := client.DoTheThing(context.TODO(), &pb.GatewayRequest{
 		Id: uuid.New().String(),
 		Stuff: &pb.Stuff{
 			Name:  "Bryan",
 			Hobby: "demos",
 			Age:   30,
 		},
-	})))
+	})
+	log.Print(response, err)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if !ok {
+			log.Print("sad")
+		}
+		log.Print(st.Details()[0].(*pb.AnError))
+	}
 }
--- cmd/target-server/main.go
+++ cmd/target-server/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"context"
 	"log"
+	"math/rand"
 	"net"
 
 	"github.com/bmoffatt/protobuf-gateway-example/must"
 	"github.com/bmoffatt/protobuf-gateway-example/pb/v2"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const (
@@ -20,9 +23,15 @@ type applicationServer struct {
 
 func (s *applicationServer) TheThing(ctx context.Context, req *pb.ApplicationRequest) (*pb.GatewayResponse, error) {
 	log.Print(req.String())
+	if rand.Int()%2 == 0 {
+		return nil, must.Return(status.New(codes.DataLoss, "IDK").WithDetails(&pb.AnError{
+			Yolo: "my BFF Jill",
+		})).Err()
+	}
 	return &pb.GatewayResponse{
 		Msg: "🤪",
 	}, nil
+
 }
 
 func init() {
